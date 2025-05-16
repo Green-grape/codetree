98920eab0a8e506ff1b407ab651c60284fd4188e
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-05-16 14:22:24 +0900

    Title: 외판원 순회, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250516/외판원 순회/traveling-salesman-problem.py
+++ 250516/외판원 순회/traveling-salesman-problem.py
@@ -1,20 +1,22 @@
-n=int(input())
+n = int(input())
 
-boards=[list(map(int,input().split())) for _ in range(n)]
-visit=[0]*n
-visit[0]=1
+boards = [list(map(int, input().split())) for _ in range(n)]
+visit = [0] * n
+visit[0] = 1
 
-ret=10000*10
+ret = 10000 * 10
+
+
 def search_min_path(idx, cur_val):
     global ret
-    if sum(visit)==n:
-        ret=min(ret, cur_val+boards[idx][0])
+    if sum(visit) == n:
+        ret = min(ret, cur_val + boards[idx][0])
         return
     for i in range(1, n):
-        if visit[i]==0:
-            visit[i]=1
-            search_min_path(i, cur_val+boards[idx][i])
-            visit[i]=0
+        if visit[i] == 0 and boards[idx][i] != 0:
+            visit[i] = 1
+            search_min_path(i, cur_val + boards[idx][i])
+            visit[i] = 0
 
 
 search_min_path(0, 0)
