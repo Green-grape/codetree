e8e0d07e08716824196cf7a683ecb7e70bd1e7a5
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-05-17 12:14:10 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250517/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,30 @@
+import os
+
+data=os.read(0, os.fstat(0).st_size).splitlines()
+ptr=0
+n, m=map(int, data[ptr].decode().split())
+ptr+=1
+
+boards=[]
+visit=[[False]*m for _ in range(n)]
+
+for i in range(n):
+    boards.append(list(map(int, data[ptr].decode().split())))
+    ptr+=1
+
+move_dirs=[(1, 0), (0, 1)]
+def dfs(i, j):
+    if visit[i][j]:
+        return
+    visit[i][j]=True
+    for dy, dx in move_dirs:
+        y=i+dy
+        x=j+dx
+        if y<0 or x<0 or x>=n or y>=n:
+            continue
+        if boards[y][x]==0:
+            continue
+        dfs(y, x)
+
+dfs(0, 0)
+print(1 if visit[n-1][m-1]==True else 0)
