bace91aa5c310b670ab8c7eef7ef3174ff7fba1f
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-05-22 13:53:51 +0900

    Title: 최소 경로로 탈출 하기, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250522/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,34 @@
+import sys
+
+input=sys.stdin.readline
+
+n, m=map(int, input().split())
+
+boards=[]
+
+for i in range(n):
+    boards.append(list(map(int, input().split())))
+
+visit=[[False]*m for _ in range(n)]
+step=[[0]*m for _ in range(n)]
+
+queue=[(0, 0, 0)]
+move_dirs=[(1, 0), (-1, 0), (0, 1), (0, -1)]
+while len(queue)>0:
+    i, j, cur_move=queue.pop(0)
+    visit[i][j]=True
+    step[i][j]=cur_move
+    for dy, dx in move_dirs:
+        y=i+dy
+        x=j+dx
+        if y<0 or x<0 or x>=m or y>=n:
+            continue
+        if not visit[y][x] and boards[y][x]==1:
+            queue.append((y, x, cur_move+1))
+            visit[y][x]=True
+
+print(step[n-1][m-1])
+        
+
+    
+    
