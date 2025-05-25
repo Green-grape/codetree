f9b3fb76f432e45ca1fc677910599bf5ced281e1
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-05-25 17:18:14 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 724ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 250525/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 250525/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -6,24 +6,39 @@ n=int(input())
 
 boards=[list(map(int, input().split())) for _ in range(n)]
 
-dp=[[(-1, 101)]*n for _ in range(n)]
+dp=[[None]*n for _ in range(n)]
 
 move_dirs=[(-1, 0), (0, -1)]
 def find_min_max_in_path(i, j):
-    # i, j까지의 경로중 최소값이 가장 작고 최대값이 가장 큰 경로의 (최소, 최대)쌍 반환
-    if dp[i][j][0]!=-1 and dp[i][j][1]!=101:
+    # i, j까지의 경로까지의 (최소, 최대) 쌍을 저장
+    if dp[i][j]!=None:
         return dp[i][j]
     if i==0 and j==0:
-        return (boards[i][j], boards[i][j])
+        return [(boards[i][j], boards[i][j])]
+    cand=[]
     for dy, dx in move_dirs:
         y=i+dy
         x=j+dx
         if 0<=x<n and 0<=y<n:
-            before_min, before_max=find_min_max_in_path(y, x)
-            cur_min, cur_max=(min(before_min, boards[i][j]), max(before_max, boards[i][j]))
-            if dp[i][j][1]-dp[i][j][0]>cur_max-cur_min:
-                dp[i][j]=(cur_min, cur_max)
+            path_list=find_min_max_in_path(y, x)
+            temp_list=[]
+            for path in path_list:
+                before_min, before_max=path
+                cur_min, cur_max=(min(before_min, boards[i][j]), max(before_max, boards[i][j]))
+                temp_list.append((cur_min, cur_max))
+            temp_list.sort(lambda x: (x[0], x[1]))
+            cur_list=[]
+            for temp in temp_list:
+                if len(cur_list)==0:
+                    cur_list.append(temp)
+                elif cur_list[-1][0]!=temp_list[0]:
+                   cur_list.append(temp)
+            cand.extend(cur_list)
+    dp[i][j]=cand
     return dp[i][j]
 
-cur_min, cur_max=find_min_max_in_path(n-1, n-1)
-print(cur_max-cur_min)
+cand_list=find_min_max_in_path(n-1, n-1)
+ret=100
+for cur_min, cur_max in cand_list:
+    ret=min(ret, cur_max-cur_min)
+print(ret)
