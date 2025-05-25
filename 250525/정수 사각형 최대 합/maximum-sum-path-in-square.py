8810035d0d0bfe9b1c99f7e93fde90003bc8348c
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-05-25 15:52:05 +0900

    Title: 정수 사각형 최대 합, Time: 66ms, Memory: 18MB, Status: Passed - Codetree

--- 250525/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 250525/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -1,25 +1,29 @@
 import sys
 
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-n=int(input())
+n = int(input())
 
-boards=[list(map(int, input().split())) for _ in range(n)]
+boards = [list(map(int, input().split())) for _ in range(n)]
 
-dp=[[0]*n for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
 
 
-move_dirs=[(-1, 0), (0, -1)]
+move_dirs = [(-1, 0), (0, -1)]
+
+
 def find_max_num(i, j):
     # 0, 0~ i,j까지 움직였을때 거쳐간 숫자들의 합의 최대값을 반환
-    if i==0 and j==0:
+    if dp[i][j] != 0:
+        return dp[i][j]
+    if i == 0 and j == 0:
         return boards[i][j]
     for dy, dx in move_dirs:
-        y=i+dy
-        x=j+dx
-        if 0<=x<n and 0<=y<n:
-            dp[i][j]=max(dp[i][j], find_max_num(y, x)+boards[i][j])
+        y = i + dy
+        x = j + dx
+        if 0 <= x < n and 0 <= y < n:
+            dp[i][j] = max(dp[i][j], find_max_num(y, x) + boards[i][j])
     return dp[i][j]
 
 
-print(find_max_num(n-1, n-1))
+print(find_max_num(n - 1, n - 1))
