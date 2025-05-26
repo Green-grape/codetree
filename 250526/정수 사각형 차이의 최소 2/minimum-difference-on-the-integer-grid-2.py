7e41dce3bab924ff5d931b461f4ecb5dce733c80
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-05-26 12:44:07 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 1421ms, Memory: 70MB, Status: Passed - Codetree

--- 250526/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 250526/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -8,6 +8,7 @@ boards = [list(map(int, input().split())) for _ in range(n)]
 
 MAX_NUM = 101
 dp = [[[None] * MAX_NUM for _ in range(n)] for _ in range(n)]
+dp2 = [[[None] * MAX_NUM for _ in range(n)] for _ in range(n)]
 
 move_dirs = [(-1, 0), (0, -1)]
 
@@ -17,25 +18,45 @@ def find_min_max_in_path(i, j, k):
     if dp[i][j][k] != None:
         return dp[i][j][k]
     if i == 0 and j == 0:
-        if boards[i][j] < k:
-            return MAX_NUM
+        if boards[i][j] >= k:
+            dp[i][j][k] = boards[i][j]
         else:
-            return boards[i][j]
+            dp[i][j][k] = MAX_NUM
+        return dp[i][j][k]
     dp[i][j][k] = MAX_NUM
-    for cand_k in range(k, MAX_NUM):
-        for dy, dx in move_dirs:
-            y = i + dy
-            x = j + dx
-            if 0 <= x < n and 0 <= y < n and boards[y][x] >= k:
-                cur_max = max(boards[i][j], find_min_max_in_path(y, x, cand_k))
-                dp[i][j][k] = min(dp[i][j][k], cur_max)
+    for dy, dx in move_dirs:
+        y = i + dy
+        x = j + dx
+        if 0 <= x < n and 0 <= y < n and boards[y][x] >= k:
+            cand = find_min_max_in_path(y, x, k)
+            if cand == MAX_NUM:
+                continue
+            cur_max = max(boards[i][j], cand)
+            dp[i][j][k] = min(dp[i][j][k], cur_max)
     return dp[i][j][k]
+
+
+def find_real_min_value_in_path(i, j, k):
+    # i, j까지의 경로에서 k이상의 값만을 사용하여 지나가는 경로들의 실제 최소값
+    if dp2[i][j][k] != None:
+        return dp2[i][j][k]
+    if i == 0 and j == 0:
+        return boards[i][j]
+    dp2[i][j][k] = boards[i][j]
+    for dy, dx in move_dirs:
+        y = i + dy
+        x = j + dx
+        if 0 <= x < n and 0 <= y < n and boards[y][x] >= k:
+            cand = find_real_min_value_in_path(y, x, k)
+            dp2[i][j][k] = min(dp2[i][j][k], cand)
+    return dp2[i][j][k]
 
 
 ret = MAX_NUM
 for k in range(1, MAX_NUM):
     cur_max = find_min_max_in_path(n - 1, n - 1, k)
+    cur_min = find_real_min_value_in_path(n - 1, n - 1, k)
     if cur_max == MAX_NUM:
         continue
-    ret = min(ret, cur_max - k)
+    ret = min(ret, cur_max - cur_min)
 print(ret)
