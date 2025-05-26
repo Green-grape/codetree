c0763af518aab013c0dcde27e631196058b247cf
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-05-26 13:16:26 +0900

    Title: 2차원 최대 증가 수열, Time: 62ms, Memory: 16MB, Status: Passed - Codetree

--- 250526/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 250526/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -14,11 +14,11 @@ def get_max_jump(i, j):
     # i, j에서 시작해서 조건을 만족하면서 밟을 수 있는 최대 칸수를 반환한다.
     if dp[i][j]!=-1:
         return dp[i][j]
-    if i==(n-1) or j==(n-1):
+    if i==(n-1) or j==(m-1):
         return 1
     dp[i][j]=1
     for y in range(i+1, n):
-        for x in range(j+1, n):
+        for x in range(j+1, m):
             if boards[i][j]<boards[y][x]:
                 dp[i][j]=max(dp[i][j], 1+get_max_jump(y, x))
     return dp[i][j]
