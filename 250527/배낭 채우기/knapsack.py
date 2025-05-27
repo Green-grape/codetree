59c8b5c020771d3034f217a878875cc6800dc369
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-05-27 15:17:50 +0900

    Title: 배낭 채우기, Time: 61ms, Memory: 17MB, Status: Passed - Codetree

--- 250527/배낭 채우기/knapsack.py
+++ 250527/배낭 채우기/knapsack.py
@@ -10,7 +10,7 @@ def get_max_jewel_value_sum(weight):
     dp=[-1]*(weight+1)
     dp[0]=0
     for jewel in jewels:
-        for w in range(weight, 1, -1):
+        for w in range(weight, 0, -1):
                 if w>=jewel[0] and dp[w-jewel[0]]!=-1:
                     dp[w]=max(dp[w], dp[w-jewel[0]]+jewel[1])
     return max(dp)
