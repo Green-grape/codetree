912e9cc499e9d03c6fb50b67c18bde6d8087aca3
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-05-27 11:39:03 +0900

    Title: 알바로 부자 되기, Time: 82ms, Memory: 25MB, Status: Passed - Codetree

--- 250527/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 250527/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -6,17 +6,21 @@ jobs=[tuple(map(int, input().split())) for _ in range(n)]
 
 jobs.sort(lambda x: (x[0], x[1], -x[2]))
 
+dp=[[-1]*n for _ in range(n)]
+
 def find_max_pay(i,cur_end):
+    if dp[i][cur_end]!=-1:
+        return dp[i][cur_end]
     if i==(n-1):
-        return jobs[i][2] if cur_end<jobs[i][0] else 0
-    ret=0
+        return jobs[i][2] if jobs[cur_end][1]<jobs[i][0] else 0
+    dp[i][cur_end]=0
     for idx in range(i+1, n):
-        if cur_end<jobs[idx][0]:
-            ret=max(ret, jobs[idx][2]+find_max_pay(idx, jobs[idx][1]))
-    return ret
+        if jobs[cur_end][1]<jobs[idx][0]:
+            dp[i][cur_end]=max(dp[i][cur_end], jobs[idx][2]+find_max_pay(idx, idx))
+    return dp[i][cur_end]
 
 ret=0
 for i in range(n):
-    ret=max(ret, find_max_pay(i, jobs[i][1])+jobs[i][2])
+    ret=max(ret, find_max_pay(i, i)+jobs[i][2])
 print(ret)
     
