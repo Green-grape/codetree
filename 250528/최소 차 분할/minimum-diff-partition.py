1448ddd1533dd6b2e27b6493d1341d8a26ef23b6
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-05-28 11:29:53 +0900

    Title: 최소 차 분할, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250528/최소 차 분할/minimum-diff-partition.py
+++ 250528/최소 차 분할/minimum-diff-partition.py
@@ -1,25 +1,26 @@
 import sys
 
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-n=int(input())
+n = int(input())
 
-nums=list(map(int, input().split()))
+nums = list(map(int, input().split()))
+
 
 def get_min_diff():
     # 현재 A그룹의 합이 a일때 이때 b의 값
-    dp=[-1]*(1000*n+1)
-    total_sum=sum(nums)
+    total_sum = sum(nums)
+    dp = [False] * (total_sum + 1)
+    dp[0] = True
     for num in nums:
-        dp[num]=total_sum-num
+        for val in range(total_sum, num - 1, -1):
+            if dp[val - num]:
+                dp[val] = True
+    ret = len(dp)
     for val in range(1, len(dp)):
-        for num in nums:
-            if val>num and dp[val-num]!=-1:
-                dp[val]=total_sum-val
-    ret=len(dp)
-    for val in range(1, len(dp)):
-        if dp[val]!=-1:
-            ret=min(ret, abs(dp[val]-val))
+        if dp[val]:
+            ret = min(ret, abs(total_sum - 2 * val))
     return ret
+
 
 print(get_min_diff())
