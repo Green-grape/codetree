81f95b3929a53fc0cff522f27e71ac56a41ecb27
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-05-28 15:13:55 +0900

    Title: 경험치를 빠르게 얻기, Time: 62ms, Memory: 17MB, Status: Passed - Codetree

--- 250528/경험치를 빠르게 얻기/gain-exp-quickly.py
+++ 250528/경험치를 빠르게 얻기/gain-exp-quickly.py
@@ -6,13 +6,17 @@ n, m=map(int, input().split())
 
 quests=[tuple(map(int, input().split())) for _ in range(n)]
 
-dp=[100*n]*(2*m+1)
+dp=[-1]*(10001) # i시간에 대하여 얻을 수 있는 최대 경험치
 dp[0]=0
 for quest in quests:
-    for exp in range(2*m, 0, -1):
-        if exp-quest[0]>=0 and dp[exp-quest[0]]!=100*n:
-            dp[exp]=min(dp[exp], dp[exp-quest[0]]+quest[1])
+    for t in range(len(dp), 0, -1):
+        if t-quest[1]>=0 and dp[t-quest[1]]!=-1:
+            dp[t]=max(dp[t], dp[t-quest[1]]+quest[0])
 
-ret=min(dp[m:])
-print(ret if ret!=100*n else -1)
+ret=-1
+for i in range(len(dp)):
+    if dp[i]>=m:
+        ret=i
+        break
+print(ret)
 
