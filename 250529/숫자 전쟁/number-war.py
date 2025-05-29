fbbc3ca816b401529f4bd5f2a31cfb83b4c911d8
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-05-29 17:26:05 +0900

    Title: 숫자 전쟁, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250529/숫자 전쟁/number-war.py
+++ 250529/숫자 전쟁/number-war.py
@@ -7,23 +7,26 @@ n=int(input())
 opponent=list(map(int, input().split()))
 me=list(map(int, input().split()))
 
-dp=[[[-1]*2 for _ in range(n)] for _ in range(n)]
+dp=[[-1]*n for _ in range(n)]
 
-def get_max_num(turn, i,j):
-    # 현재 turn에 상대의 맨 앞의 수가 i번째, 나의 맨 앞의 수가 j번째일때 게임이 끝났을때 얻을 수 있는 최대 점수는?
+    
+def get_max_num(i,j):
+    # 상대의 맨 앞의 수가 i번째, 나의 맨 앞의 수가 j번째일때 게임이 끝났을때 얻을 수 있는 최대 점수는?
     if i==n or j==n: # 게임 종료
         return 0;
-    if dp[i][j][turn]!=-1:
-        return dp[i][j][turn]
+    if dp[i][j]!=-1:
+        return dp[i][j]
 
     # 1번 rule
-    dp[i][j][turn]=0
+    dp[i][j]=0
     if opponent[i]>me[j]:
-        dp[i][j][turn]=max(dp[i][j][turn], me[j]+get_max_num((turn+1)%2, i, j+1))
+        dp[i][j]=max(dp[i][j], me[j]+get_max_num(i, j+1))
     elif opponent[i]<me[j]:
-        dp[i][j][turn]=max(dp[i][j][turn], get_max_num((turn+1)%2, i+1, j))
-    dp[i][j][turn]=max(dp[i][j][turn], get_max_num((turn+1)%2, i+1, j+1))
-    return dp[i][j][turn]
+        dp[i][j]=max(dp[i][j], get_max_num(i+1, j))
 
-print(get_max_num(0, 0, 0))
+    #2번 rule
+    dp[i][j]=max(dp[i][j], get_max_num(i+1, j+1))
+    return dp[i][j]
+
+print(get_max_num(0, 0))
 
