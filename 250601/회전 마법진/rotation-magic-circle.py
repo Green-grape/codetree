382891b0f246ca91ef94e5c8529fe56fd4adcf2a
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-06-01 17:30:47 +0900

    Title: 회전 마법진, Time: 100ms, Memory: 23MB, Status: Wrong Answer - Codetree

--- 250601/회전 마법진/rotation-magic-circle.py
+++ 250601/회전 마법진/rotation-magic-circle.py
@@ -4,19 +4,25 @@ input=sys.stdin.readline
 
 n=int(input())
 
-before_state=[int(s) for s in input()[:-1]]
-after_state=[int(s) for s in input()[:-1]]
+before_state=[int(s) for s in input().strip()]
+after_state=[int(s) for s in input().strip()]
 
-ret=sys.maxsize
-for rev in range(10):
-    temp=rev
-    for i in range(n):
-        temp+=abs((before_state[i]+rev)%10-after_state[i])
-    ret=min(ret, temp)
-print(ret)
-        
+dp=[[sys.maxsize]*11 for _ in range(n)]
+
+def get_min_rotate(idx, rev_rotate_cnt):
+    # 지금 idx번째 마법진을 체크할 차례이고 지금까지 반시계로 회전한 횟수가 rev_rotate_cnt일때 원하는 state에 도달하기 위한 
+    # 최소 회전 횟수는?
+    if idx==n:
+        return 0
+    if dp[idx][rev_rotate_cnt]<sys.maxsize:
+        return dp[idx][rev_rotate_cnt]
+    # 시계방향 회전
+    cur_diff=abs((before_state[idx]+rev_rotate_cnt)%10-after_state[idx])
+    dp[idx][rev_rotate_cnt]=min(dp[idx][rev_rotate_cnt], cur_diff+get_min_rotate(idx+1, rev_rotate_cnt))
+
+    # 반시계 회전
+    dp[idx][rev_rotate_cnt]=min(dp[idx][rev_rotate_cnt], cur_diff+get_min_rotate(idx+1, (rev_rotate_cnt+cur_diff)%10))
+    return dp[idx][rev_rotate_cnt]
 
 
-# 0 1 2...9
-# 1 2 ... 9 0
-# 9 0 1 2 ...8
+print(get_min_rotate(0, 0))
