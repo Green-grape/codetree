bda268693b59dd1e378037fdf33ab6275c88c6c8
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-06-02 17:18:53 +0900

    Title: 최장 공통 부분 수열 3, Time: 92ms, Memory: 22MB, Status: Wrong Answer - Codetree

--- 250602/최장 공통 부분 수열 3/longest-common-sequence-3.py
+++ 250602/최장 공통 부분 수열 3/longest-common-sequence-3.py
@@ -1,46 +1,39 @@
 import sys
 
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-la, lb = map(int, input().split())
+la, lb=map(int, input().split())
 
-arr_a = list(map(int, input().split()))
-arr_b = list(map(int, input().split()))
+arr_a=list(map(int, input().split()))
+arr_b=list(map(int, input().split()))
 
-dp = [[-1] * (lb + 1) for _ in range(la + 1)]
-dp[0][0] = 0
+dp=[[-1]*(lb+1) for _ in range(la+1)]
+dp[0][0]=0
 
-for i in range(la + 1):
-    for j in range(lb + 1):
-        if dp[i][j] == -1:
+for i in range(la+1):
+    for j in range(lb+1):
+        if dp[i][j]==-1:
             continue
-        if i < la and j < lb and arr_a[i] == arr_b[j]:
-            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], 1 + dp[i][j])
-        if i < la:
-            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j])
-        if j < lb:
-            dp[i][j + 1] = max(dp[i][j + 1], dp[i][j])
+        if i<la and j<lb and arr_a[i]==arr_b[j]:
+            dp[i+1][j+1]=max(dp[i+1][j+1], 1+dp[i][j])
+        if i<la:
+            dp[i+1][j]=max(dp[i+1][j], dp[i][j])
+        if j<lb:
+            dp[i][j+1]=max(dp[i][j+1], dp[i][j])
 
-
-def dfs(i, j):
-    if i == la or j == lb:
-        return []
-    if arr_a[i] == arr_b[j]:
-        return [arr_a[i]] + dfs(i + 1, j + 1)
-    if dp[i + 1][j] == dp[i][j + 1]:
-        s1 = dfs(i + 1, j)
-        s2 = dfs(i, j + 1)
-        if len(s1) > len(s2):
-            return s1
-        elif len(s1) < len(s2):
-            return s2
-        else:
-            return min(s1, s2)
-    elif dp[i + 1][j] > dp[i][j + 1]:
-        return dfs(i + 1, j)
+i, j=la, lb
+ret=[]
+while i>0 and j>0:
+    if arr_a[i-1]==arr_b[j-1]:
+        ret.insert(0, arr_a[i-1])
+        i-=1
+        j-=1
+    elif dp[i-1][j]==dp[i][j-1]:
+        if arr_a[i-1]>arr_b[j-1]: i-=1
+        else: j-=1
+    elif dp[i-1][j]>dp[i][j-1]:
+        i-=1
     else:
-        return dfs(i, j + 1)
+        j-=1
 
-
-ret = dfs(0, 0)
-print(" ".join(map(str, ret)))
+print(" ".join([str(num) for num in ret]))
