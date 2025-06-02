cd7b6d2f2b4b6730fa744436b155ef5e71315004
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-06-02 14:06:11 +0900

    Title: 최장 공통 부분 수열, Time: 1098ms, Memory: 42MB, Status: Time Limit Exceed - Codetree

--- 250602/최장 공통 부분 수열/longest-common-sequence.py
+++ 250602/최장 공통 부분 수열/longest-common-sequence.py
@@ -6,16 +6,14 @@ dp=[[0]*len(str_b) for _ in range(len(str_a))]
 
 def get_max_superseq_cnt(i, j):
     # 현재 a에서는 i번째 b에서는 j번째 문자열을 확인할때 최장 공통 수열의 길이는?
-    if i==0 and j==0:
-        return 1 if str_a[i]==str_b[j] else 0
+    if i<0 or j<0:
+        return 0
     if dp[i][j]!=0:
         return dp[i][j]
-    if (i>0 and j>0) and str_a[i]==str_b[j]:
-        dp[i][j]=1+get_max_superseq_cnt(i-1, j-1)
-    if i>0:
-        dp[i][j]=max(dp[i][j], get_max_superseq_cnt(i-1, j))
-    if j>0:
-        dp[i][j]=max(dp[i][j], get_max_superseq_cnt(i, j-1))
+    is_same=int(str_a[i]==str_b[j])
+    dp[i][j]=max(dp[i][j], is_same+get_max_superseq_cnt(i-1, j-1))
+    dp[i][j]=max(dp[i][j], get_max_superseq_cnt(i-1, j))
+    dp[i][j]=max(dp[i][j], get_max_superseq_cnt(i, j-1))
     return dp[i][j]
 
 print(get_max_superseq_cnt(len(str_a)-1,len(str_b)-1))
