e6313dc677d796214560ba504c0f8d2440882fc2
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-06-02 14:07:16 +0900

    Title: 최장 공통 부분 수열, Time: 1035ms, Memory: 41MB, Status: Time Limit Exceed - Codetree

--- 250602/최장 공통 부분 수열/longest-common-sequence.py
+++ 250602/최장 공통 부분 수열/longest-common-sequence.py
@@ -2,13 +2,13 @@ import sys
 str_a=input()
 str_b=input()
 
-dp=[[0]*len(str_b) for _ in range(len(str_a))]
+dp=[[-1]*len(str_b) for _ in range(len(str_a))]
 
 def get_max_superseq_cnt(i, j):
     # 현재 a에서는 i번째 b에서는 j번째 문자열을 확인할때 최장 공통 수열의 길이는?
     if i<0 or j<0:
         return 0
-    if dp[i][j]!=0:
+    if dp[i][j]!=-1:
         return dp[i][j]
     is_same=int(str_a[i]==str_b[j])
     dp[i][j]=max(dp[i][j], is_same+get_max_superseq_cnt(i-1, j-1))
