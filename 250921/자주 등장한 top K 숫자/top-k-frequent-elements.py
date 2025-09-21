432a63ea3814a6ddd82331251fac0ec85128edfa
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-09-21 22:02:16 +0900

    Title: 자주 등장한 top K 숫자, Time: 75ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250921/자주 등장한 top K 숫자/top-k-frequent-elements.py
@@ -0,0 +1,14 @@
+n, m=map(int, input().split())
+
+num_list=map(int, input().split())
+cnt_dict={}
+for num in num_list:
+    if num in cnt_dict:
+        cnt_dict[num]=(cnt_dict[num][1]+1, num)
+    else:
+        cnt_dict[num]=(1, num)
+
+sorted_value=sorted(list(cnt_dict.values()), reverse=True)
+
+ret=" ".join([str(sorted_value[i][1]) for i in range(m)])
+print(ret)
