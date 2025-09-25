02f179119e2c2911f5e74ea8adca5c7ca67beba4
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-09-25 23:43:18 +0900

    Title: 데이터 비교, Time: 1059ms, Memory: 39MB, Status: Time Limit Exceed - Codetree

---
+++ 250925/데이터 비교/data-comparison.py
@@ -0,0 +1,11 @@
+n=int(input())
+n1_set=set(map(int, input().split()))
+m=int(input())
+m1_list=map(int, input().split())
+
+ret=""
+for num in m1_list:
+    if num in n1_set: ret+="1 "
+    else: ret+="0 "
+print(ret)
+
