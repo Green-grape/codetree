bbdd12b6438aa12cd3296a97adcb18f6782f30a8
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-09-27 16:09:35 +0900

    Title: C-TAG, Time: 339ms, Memory: 20MB, Status: Passed - Codetree

--- 250927/C-TAG/c-tag.py
+++ 250927/C-TAG/c-tag.py
@@ -8,12 +8,14 @@ b_group_list=[input() for _ in range(n)]
 ret=0
 for (i1, i2, i3) in itertools.combinations(range(m), 3):
     a_set=set()
+    b_set=set()
     con=False
     for i in range(n):
         a_key=a_group_list[i][i1]+a_group_list[i][i2]+a_group_list[i][i3]
         b_key=b_group_list[i][i1]+b_group_list[i][i2]+b_group_list[i][i3]
         a_set.add(a_key)
-        if b_key in a_set:
+        b_set.add(b_key)
+        if b_key in a_set or a_key in b_set:
             con=True
             break
     if con:
