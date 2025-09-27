57f5e0a9d5392c100ef3f4b1b8551bc7275d5510
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-09-27 16:06:55 +0900

    Title: C-TAG, Time: 86ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250927/C-TAG/c-tag.py
@@ -0,0 +1,23 @@
+import itertools
+
+n, m=map(int, input().split())
+
+a_group_list=[input() for _ in range(n)]
+b_group_list=[input() for _ in range(n)]
+
+ret=0
+for (i1, i2, i3) in itertools.combinations(range(m), 3):
+    a_set=set()
+    con=False
+    for i in range(n):
+        a_key=a_group_list[i][i1]+a_group_list[i][i2]+a_group_list[i][i3]
+        b_key=b_group_list[i][i1]+b_group_list[i][i2]+b_group_list[i][i3]
+        a_set.add(a_key)
+        if b_key in a_set:
+            con=True
+            break
+    if con:
+        continue
+    ret+=1
+
+print(ret)
