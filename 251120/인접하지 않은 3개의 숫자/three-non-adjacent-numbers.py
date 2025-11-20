7fd7b172ad6c12c2e8d74325ed0fc1cdd89f0546
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-11-20 14:12:05 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 61ms, Memory: 22MB, Status: Wrong Answer - Codetree

---
+++ 251120/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+nums = list(map(int, input().split()))
+
+max_num_lefts = [0] * n
+max_num_rights = [0] * n
+
+max_num_lefts[0] = nums[0]
+for i in range(1, n):
+    max_num_lefts[i] = max(max_num_lefts[i - 1], nums[i - 1])
+
+max_num_rights[n - 1] = nums[n - 1]
+for i in range(n - 2, -1, -1):
+    max_num_rights[i] = max(max_num_rights[i + 1], nums[i + 1])
+
+ret = 0
+for j in range(2, n - 2):
+    left_max = max_num_lefts[j - 2]
+    right_max = max_num_rights[j + 2]
+    ret = max(0, left_max + right_max + nums[j])
+print(ret)
