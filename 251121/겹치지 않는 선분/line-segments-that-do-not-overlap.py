f18612bfa0d5a25841d37bfb1c8d73d05876c90b
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-11-21 17:12:56 +0900

    Title: 겹치지 않는 선분, Time: 192ms, Memory: 24MB, Status: Passed - Codetree

--- 251121/겹치지 않는 선분/line-segments-that-do-not-overlap.py
+++ 251121/겹치지 않는 선분/line-segments-that-do-not-overlap.py
@@ -8,13 +8,13 @@ for _ in range(n):
 coords.sort()
 
 left_x2_max = [0] * n  # left_x2_max[i] = 0~i까지 x2의 최댓값
-right_x2_min = [0] * n  # right_x2_min[i] = i~n-1까지 x2의 최솟값
+right_x2_min = [0] * n  # right_x2_min[i] = i~n-1까지 x1의 최솟값
 
 left_x2_max[0] = coords[0][1]
 for i in range(1, n):
     left_x2_max[i] = max(left_x2_max[i - 1], coords[i][1])
 
-right_x2_min[n - 1] = coords[n - 1][0]
+right_x2_min[n - 1] = coords[n - 1][1]
 for i in range(n - 2, -1, -1):
     right_x2_min[i] = min(right_x2_min[i + 1], coords[i][1])
 
