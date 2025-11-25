a4ebad2409bbc8fbb8c11cddeb213144d860e641
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-11-25 13:36:28 +0900

    Title: 구간 크기의 합, Time: 187ms, Memory: 23MB, Status: Passed - Codetree

---
+++ 251125/구간 크기의 합/sum-of-interval-sizes.py
@@ -0,0 +1,30 @@
+n = int(input())
+
+points = []
+
+for i in range(n):
+    a, b = map(int, input().split())
+    points.append((a, i, 1))
+    points.append((b, i, -1))
+
+points.sort()
+
+remain_line_count = 0
+cur_min = None
+cur_max = None
+
+ret = 0
+for x, idx, v in points:
+    if v == 1:
+        if remain_line_count == 0:
+            cur_min = x
+        remain_line_count += 1
+    else:
+        remain_line_count -= 1
+        if remain_line_count == 0:
+            cur_max = x
+            ret += cur_max - cur_min
+            cur_min = None
+            cur_max = None
+
+print(ret)
