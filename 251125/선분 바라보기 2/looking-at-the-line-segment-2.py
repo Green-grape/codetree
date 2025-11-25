4ab2ddd55d166a8bf63cc9545281d0a8425e81fe
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-11-25 18:31:48 +0900

    Title: 선분 바라보기 2, Time: 843ms, Memory: 39MB, Status: Passed - Codetree

--- 251125/선분 바라보기 2/looking-at-the-line-segment-2.py
+++ 251125/선분 바라보기 2/looking-at-the-line-segment-2.py
@@ -1,33 +1,33 @@
 from sortedcontainers import SortedList
 
+# f = open("./test_case.txt", "r")
+# input = f.readline
+
 n = int(input())
 points = []
 
 for i in range(n):
     y, x1, x2 = map(int, input().split())
-    points.append((x1, y, 1))  # start
-    points.append((x2, y, -1))  # end
+    points.append((x1, +1, i, y))
+    points.append((x2, -1, i, y))
 
-# x 기준 정렬
 points.sort()
 
 y_set = SortedList()
 ret = 0
 prev_min_y = None
 
-for x, y, typ in points:
-    if typ == 1:  # interval enters
-        y_set.add(y)
-    else:  # interval leaves
-        y_set.remove(y)
+visited = [False] * n
 
-    # 현재 최소 y 계산
-    curr_min_y = y_set[0] if y_set else None
+for x, typ, idx, y in points:
+    if typ == 1:
+        y_set.add((y, idx))
+    else:
+        y_set.remove((y, idx))
+    if not y_set:
+        continue
+    _, target_idx = y_set[0]
+    visited[target_idx] = True
 
-    # 변화 감지
-    if curr_min_y != prev_min_y and curr_min_y is not None:
-        ret += 1
-
-    prev_min_y = curr_min_y
-
-print(ret)
+print(sum(visited))
+ 
