d1b19c74fe2cc64028c9752e0914f2f5764f7f73
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-11-26 11:45:02 +0900

    Title: 호텔 예약, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251126/호텔 예약/reserve-hotel.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+points = []
+
+for i in range(n):
+    a, b = map(int, input().split())
+    points.append((a, i, 1))
+    points.append((b, i, -1))
+
+
+cur_concurrency = 0
+max_concurrency = 0
+for point in sorted(points):
+    cur_concurrency += point[2]
+    max_concurrency = max(max_concurrency, cur_concurrency)
+print(max_concurrency)
