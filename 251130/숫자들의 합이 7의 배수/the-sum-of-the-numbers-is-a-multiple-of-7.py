8aa055b5df80964ca4a62ee32a37a194ccf1eaf3
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-11-30 14:50:54 +0900

    Title: 숫자들의 합이 7의 배수, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251130/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+nums = []
+for _ in range(n):
+    nums.append(int(input()))
+
+MOD = 7
+
+prefix_sums = [0] * (n + 1)
+for i in range(n):
+    prefix_sums[i + 1] = ((prefix_sums[i] % MOD) + (nums[i] % MOD)) % MOD
+
+remain_min_idx = [
+    float("inf")
+] * MOD  # remain_min_idx[i]: prefix_sums % MOD == i의 최소 인덱스
+remain_max_idx = [-1] * MOD  # remain_max_idx[i]: prefix_sums % MOD == i의 최대 인덱스
+
+for i in range(1, n + 1):
+    remain = prefix_sums[i] % MOD
+    remain_min_idx[remain] = min(remain_min_idx[remain], i)
+    remain_max_idx[remain] = max(remain_max_idx[remain], i)
+
+ans = 0
+for i in range(MOD):
+    if remain_max_idx[i] != -1:
+        ans = max(ans, remain_max_idx[i] - remain_min_idx[i])
+print(ans)
+ 
