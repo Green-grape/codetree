0483e01acee87d9f0511b565ee734a1cfcd7b706
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-12-01 22:50:56 +0900

    Title: 0에 가장 가까운 합, Time: 80ms, Memory: 20MB, Status: Passed - Codetree

--- 251201/0에 가장 가까운 합/sum-closest-to-zero.py
+++ 251201/0에 가장 가까운 합/sum-closest-to-zero.py
@@ -1,21 +1,18 @@
 n = int(input())
-
 nums = list(map(int, input().split()))
 
 nums.sort()
 
+i = 0
 j = n - 1
-cur_close_to_zero = float("inf")
-most_close_to_zero = float("inf")
-for i in range(n):
-    prev_num = cur_close_to_zero
-    while j > i and abs(nums[i] + nums[j]) <= abs(cur_close_to_zero):
-        cur_close_to_zero = nums[i] + nums[j]
-        if abs(cur_close_to_zero) > abs(prev_num):
-            cur_close_to_zero = prev_num
-            break
-        j -= 1
-    if abs(cur_close_to_zero) < abs(most_close_to_zero):
-        most_close_to_zero = cur_close_to_zero
+best = float("inf")
 
-print(abs(most_close_to_zero))
+while i < j:
+    s = nums[i] + nums[j]
+    if abs(s) < abs(best):
+        best = s
+    if s < 0:
+        i += 1
+    else:
+        j -= 1
+print(abs(best))
