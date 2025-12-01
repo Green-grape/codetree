0d45536d6116b1c50cafeaaaba3cca7e08cb1542
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-12-01 23:20:34 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 300ms, Memory: 26MB, Status: Passed - Codetree

--- 251201/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
+++ 251201/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
@@ -1,3 +1,7 @@
+from sortedcontainers import SortedList, SortedSet
+
+# input = open("./test_case.txt", "r").readline
+
 n, d = map(int, input().split())
 
 coords = []
@@ -10,14 +14,18 @@ coords.sort()
 
 j = 1
 x_min_diff = float("inf")
+sorted_y = SortedList()
+is_inserted = [False] * n
 for i in range(n):
-    y_min = coords[i][1]
-    y_max = coords[i][1]
-    while j < n and y_max - y_min <= d:
-        y_max = max(y_max, coords[j][1])
-        y_min = min(y_min, coords[j][1])
+    if not is_inserted[i]:
+        sorted_y.add(coords[i][1])
+    while j < n and sorted_y[-1] - sorted_y[0] < d:
+        if not is_inserted[j]:
+            is_inserted[j] = True
+            sorted_y.add(coords[j][1])
         j += 1
-    if y_max - y_min >= d:
+    if sorted_y[-1] - sorted_y[0] >= d:
         x_min_diff = min(x_min_diff, coords[j - 1][0] - coords[i][0])
+    sorted_y.remove(coords[i][1])
 
 print(x_min_diff if x_min_diff != float("inf") else -1)
