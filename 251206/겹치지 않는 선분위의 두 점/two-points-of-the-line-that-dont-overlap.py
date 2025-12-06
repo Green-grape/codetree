9ef685a3179cdfa5174352c8d87b6e465378a380
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-12-06 19:00:17 +0900

    Title: 겹치지 않는 선분위의 두 점, Time: 1140ms, Memory: 28MB, Status: Passed - Codetree

--- 251206/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.py
+++ 251206/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.py
@@ -1,3 +1,4 @@
+
 n, m = map(int, input().split())
 
 lines = [tuple(map(int, input().split())) for _ in range(m)]
@@ -10,12 +11,20 @@ right = 10**18
 while left <= right:
     mid = (left + right) // 2
 
-    count = 1
-    prev = lines[0][0]
+    count = 0
+    prev = -1
     for a, b in lines:
-        count += (b - prev) // mid
-        if (b - prev) // mid > 0:
-            prev += ((b - prev) // mid) * mid
+        if count == 0:
+            pos = a
+        else:
+            pos = max(a, prev + mid)
+
+        if pos > b:
+            continue
+        cnt = (b - pos) // mid + 1
+        count += cnt
+        prev = pos + (cnt - 1) * mid
+
     if count >= n:
         left = mid + 1
     else:
