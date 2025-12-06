cf14fbfd0904a79006adfd1de4dc5fb2d99684db
Kim juho <60733414+Green-grape@users.noreply.github.com>
2025-12-06 19:26:56 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 51ms, Memory: 17MB, Status: Passed - Codetree

--- 251206/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 251206/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -2,18 +2,12 @@ n = int(input())
 
 nums = list(map(int, input().split()))
 
-max_substr_sum = max(nums)
 prev_sum = 0
-prev_cnt = 0
+max_substr_sum = -float("inf")
 for i in range(n):
-    if nums[i] < 0:
-        if prev_cnt > 0:
-            max_substr_sum = max(max_substr_sum, prev_sum)
+    prev_sum += nums[i]
+    if prev_sum > max_substr_sum:
+        max_substr_sum = prev_sum
+    if prev_sum < 0:
         prev_sum = 0
-        prev_cnt = 0
-    else:
-        prev_sum += nums[i]
-        prev_cnt += 1
-if prev_cnt > 0:
-    max_substr_sum = max(max_substr_sum, prev_sum)
 print(max_substr_sum)
